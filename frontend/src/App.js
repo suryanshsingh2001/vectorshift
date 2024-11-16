94247c30b6aa892cfd334c8837b87e54b40fa374
suryanshsingh2001 <ricochetthestoryteller2001@gmail.com>
2024-11-16 19:02:27 +0530

    (Add) Padding

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <PipelineToolbar />
-      <main className="flex-grow p-4 ">
+      <main className="flex-grow ">
         <PipelineUI />
         <SubmitButton />
 
